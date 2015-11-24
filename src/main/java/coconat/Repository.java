ca172689f55e5e857c0855d2dad60b9c19bb0d7b
Martin Goellnitz <martin@provocon.eu>
2015-11-24 23:59:09 +0100

    Enhance JavaDoc

--- src/main/java/coconat/Repository.java
+++ src/main/java/coconat/Repository.java
@@ -1,20 +1,20 @@
 /*
- * 
+ *
  * Copyright 2015 Martin Goellnitz
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 package coconat;
 
@@ -25,9 +25,9 @@ package coconat;
 public interface Repository {
 
     /**
-     * retrieves item with the given id from the repository.
+     * Retrieves a content item with a given id from the repository.
      *
-     * @param id
+     * @param id content id if the content to retrieve
      * @return respective content item or null if not available
      */
     Content getContent(String id);
