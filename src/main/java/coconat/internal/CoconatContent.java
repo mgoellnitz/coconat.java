b0b2a0f398105d5e425a4d862e93e2e02b16a2aa
Martin Goellnitz <martin@provocon.eu>
2016-03-17 21:00:59 +0100

    Tweak test line coverage

--- src/main/java/coconat/internal/CoconatContent.java
+++ src/main/java/coconat/internal/CoconatContent.java
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright 2011-2014 Martin Goellnitz
+ * Copyright 2011-2016 Martin Goellnitz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -54,17 +54,10 @@ public class CoconatContent implements Content, Map<String, Object> {
     }
 
 
-    public Object get(String name) {
-        return properties.get(name);
-    } // getProperty()
+    /*
+     * Map Interface
+     */
 
-
-    public void set(String name, Object value) {
-        properties.put(name, value);
-    } // set()
-
-
-    /** Map * */
     @Override
     public void clear() {
         properties.clear();
